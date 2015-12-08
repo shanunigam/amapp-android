4205d6ea9a1b134cd162486930db58b2e1e1ca1b
Shanu Nigam <shanu.nigam@gmail.com>
2015-12-07 22:50:53 -0800

    Updated the mock url to point to Apiary.io

--- AMApp/src/com/amapp/common/AMConstants.java
+++ AMApp/src/com/amapp/common/AMConstants.java
@@ -5,8 +5,8 @@ package com.amapp.common;
  */
 public class AMConstants {
     //MOCK
-    public static final String MOCK_Domain_Url = "http://www.mocky.io";
-    public static final String MOCK_ThakorjiToday_Endpoint_Suffix = "/v2/565320442400005027629a41";
+    public static final String MOCK_Domain_Url = "http://private-c5b4e-ams4.apiary-mock.com";
+    public static final String MOCK_ThakorjiToday_Endpoint_Suffix = "/api/ams/v1/thakorji.php";
     public static final String MOCK_QuoteOfTheWeek_Endpoint_Suffix = "/v2/5665890a250000602c996e79";
 
     //LIVE
